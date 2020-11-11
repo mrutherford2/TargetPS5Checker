9fb7ed28cb873bc459dee010316f6143e12212b7
MR <myq7@cdc.gov>
2020-11-11 16:59:06 -0500

    initial commit

---
+++ TargetAvailabilityChecker/FullfillmentResponse.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TargetAvailabilityChecker
+{
+    public class OrderPickup
+    {
+        [JsonProperty("availability_status")]
+        public string AvailabilityStatus { get; set; }
+    }
+
+    public class Curbside
+    {
+        [JsonProperty("availability_status")]
+        public string AvailabilityStatus { get; set; }
+    }
+
+    public class ShipToStore
+    {
+        [JsonProperty("availability_status")]
+        public string AvailabilityStatus { get; set; }
+    }
+
+    public class InStoreOnly
+    {
+        [JsonProperty("availability_status")]
+        public string AvailabilityStatus { get; set; }
+    }
+
+    public class Location
+    {
+        [JsonProperty("location_id")]
+        public string LocationId { get; set; }
+
+        [JsonProperty("distance")]
+        public string Distance { get; set; }
+
+        [JsonProperty("store_name")]
+        public string StoreName { get; set; }
+
+        [JsonProperty("store_address")]
+        public string StoreAddress { get; set; }
+
+        [JsonProperty("location_available_to_promise_quantity")]
+        public double LocationAvailableToPromiseQuantity { get; set; }
+
+        [JsonProperty("order_pickup")]
+        public OrderPickup OrderPickup { get; set; }
+
+        [JsonProperty("curbside")]
+        public Curbside Curbside { get; set; }
+
+        [JsonProperty("ship_to_store")]
+        public ShipToStore ShipToStore { get; set; }
+
+        [JsonProperty("in_store_only")]
+        public InStoreOnly InStoreOnly { get; set; }
+    }
+
+    public class Product
+    {
+        [JsonProperty("product_id")]
+        public string ProductId { get; set; }
+
+        [JsonProperty("street_date")]
+        public DateTime StreetDate { get; set; }
+
+        [JsonProperty("available_to_purchase_date_time")]
+        public DateTime AvailableToPurchaseDateTime { get; set; }
+
+        [JsonProperty("buy_unit_of_measure")]
+        public string BuyUnitOfMeasure { get; set; }
+
+        [JsonProperty("availability_unit_of_measure")]
+        public string AvailabilityUnitOfMeasure { get; set; }
+
+        [JsonProperty("locations")]
+        public List<Location> Locations { get; set; }
+    }
+
+    public class FullfillmentResponse
+    {
+        [JsonProperty("products")]
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ TargetAvailabilityChecker/Program.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TargetAvailabilityChecker
+{
+    /// <summary>
+    /// Simple App to check for PS5 availablility at Target
+    /// </summary>
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter Zip Code: ");
+            string zipCode = Console.ReadLine();
+
+            Console.WriteLine("Enter Key: ");
+            string key = Console.ReadLine();
+
+            int checkInterval = 2000;
+            bool inStock = false;
+            const int digitalEditionProductId = 81114596;
+            const int discEditionProductId = 81114595;
+
+            using var digitalEditionClient = new HttpClient();
+            string digitalEditionUri = $"https://api.target.com/fulfillment_aggregator/v1/fiats/{digitalEditionProductId}?key={key}&nearby={zipCode}&limit=20&requested_quantity=1&radius=50&fulfillment_test_mode=grocery_opu_team_member_test";
+
+            using var discEditionClient = new HttpClient();
+            string discEditionUri = $"https://api.target.com/fulfillment_aggregator/v1/fiats/{discEditionProductId}?key={key}&nearby={zipCode}&limit=20&requested_quantity=1&radius=50&fulfillment_test_mode=grocery_opu_team_member_test";
+            while (!inStock)
+            {
+                Console.WriteLine("Checking for available stock");
+                var digitalEditionResponseTask = digitalEditionClient.GetAsync(digitalEditionUri);
+                var discEditionResponseTask = discEditionClient.GetAsync(discEditionUri);
+
+                await Task.WhenAll(digitalEditionResponseTask, discEditionResponseTask);
+
+                var digitalEditionResponse = digitalEditionResponseTask.Result;
+                var discEditionResponse = discEditionResponseTask.Result;
+
+                if (digitalEditionResponse.IsSuccessStatusCode)
+                {
+                    var digitalEditionData = JsonConvert.DeserializeObject<FullfillmentResponse>(await digitalEditionResponse.Content.ReadAsStringAsync());
+                    var closestDigitalEditionInStock = digitalEditionData.Products?.FirstOrDefault()?.Locations?.OrderBy(x => x.Distance)?
+                        .Where(x => x.Curbside.AvailabilityStatus != "UNAVAILABLE" || x.OrderPickup.AvailabilityStatus != "UNAVAILABLE" || x.ShipToStore.AvailabilityStatus != "UNAVAILABLE" || x.InStoreOnly.AvailabilityStatus != "UNAVAILABLE")?.FirstOrDefault();
+                    if(closestDigitalEditionInStock != null)
+                    {
+                        Console.Beep();
+                        Console.WriteLine($"Digital Edition Available at Store Name: {closestDigitalEditionInStock.StoreName} Store Address: {closestDigitalEditionInStock.StoreAddress} Amount Available: {closestDigitalEditionInStock.LocationAvailableToPromiseQuantity}");
+                        inStock = true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Got non-successful response back from Target API. Will attempt to check again.");
+                }
+
+                if (discEditionResponse.IsSuccessStatusCode)
+                {
+                    var discEditionData = JsonConvert.DeserializeObject<FullfillmentResponse>(await discEditionResponse.Content.ReadAsStringAsync());
+                    var closestDiscEditionInStock = discEditionData.Products?.FirstOrDefault()?.Locations?.OrderBy(x => x.Distance)?
+                        .Where(x => x.Curbside.AvailabilityStatus != "UNAVAILABLE" || x.OrderPickup.AvailabilityStatus != "UNAVAILABLE" || x.ShipToStore.AvailabilityStatus != "UNAVAILABLE" || x.InStoreOnly.AvailabilityStatus != "UNAVAILABLE")?.FirstOrDefault();
+                    if(closestDiscEditionInStock != null)
+                    {
+                        Console.Beep();
+                        Console.WriteLine($"Disc Edition Available at Store Name: {closestDiscEditionInStock.StoreName} Store Address: {closestDiscEditionInStock.StoreAddress} Amount Available: {closestDiscEditionInStock.LocationAvailableToPromiseQuantity}");
+                        inStock = true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Got non-successful response back from Target API. Will attempt to check again.");
+                }
+
+                Console.WriteLine("No stock available, trying again");
+                Thread.Sleep(checkInterval);
+            }
+
+        }
+    }
+}
